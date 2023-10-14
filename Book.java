c423c879a6f3c8180b5aa5b2b881ad9f414cc55b
hironori-y <147683736+hironori-y@users.noreply.github.com>
2023-10-14 11:18:54 +0900

    Delete Book.java

--- Book.java
+++
@@ -1,9 +0,0 @@
-public class Book extends TangibleAsset{
-	private String isbn;
-	public Book(string name,int price,String color,String isbn){
-		super(name,price,color);
-		this.isbn = isbn;
-	}
-
-	pubic String getlsbn(){ return this.isbn;}
-}
